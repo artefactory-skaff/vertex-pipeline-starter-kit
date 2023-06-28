6824bbda6749ae3d6ffeec0b6b96e1d743683e41
julesbertrand <julesbertrand13@gmail.com>
2023-06-28 17:36:13 +0200

    fix: load_config should take two arguments: pipeline_name and config_name

--- vertex/lib/utils/config.py
+++ vertex/lib/utils/config.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from typing import Dict
 import json
 
-
-def load_config(config_name: str) -> Dict:
-    with open(Path(__file__).parent.parent.parent / "configs" / "my_first_pipeline" / f"{config_name}.json") as f:
+def load_config(pipeline_name: str, config_name: str) -> Dict:
+    config_filepath = Path(__file__).parent.parent.parent / "configs" / pipeline_name / f"{config_name}.json"
+    with open(config_filepath) as f:
         config = json.load(f)
     return config
