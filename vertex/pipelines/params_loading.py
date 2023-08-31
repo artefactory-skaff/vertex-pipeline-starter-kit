dca1b77544cd6b784462043d776970f252e990e8
Alexis VIALARET <alexis.vialaret@artefact.com>
2023-08-31 15:25:34 +0200

    doc: removed stray changes

--- vertex/pipelines/params_loading.py
+++ vertex/pipelines/params_loading.py
@@ -1,4 +1,7 @@
+import json
 import os
+from pathlib import Path
+from typing import Dict
 
 import kfp
 from kfp.v2 import compiler
@@ -6,14 +9,25 @@ import google.cloud.aiplatform as aip
 from kfp.v2.dsl import component
 
 
-@component(base_image="python3.9")
-def dummy_task():
+@component(base_image=f'eu.gcr.io/{os.getenv("PROJECT_ID")}/vertex-pipelines-base:latest')
+def dummy_task(project_id: str, country: str, start_date: str, end_date: str):
     pass
 
 
 @kfp.dsl.pipeline(name="parametrized-pipeline")
-def pipeline(config_name: str):
-    pass
+def pipeline(project_id: str, country: str, start_date: str, end_date: str):
+    dummy_task(
+        project_id=project_id,
+        country=country,
+        start_date=start_date,
+        end_date=end_date
+    )
+
+
+def load_config(config_name: str) -> Dict:
+    with open(Path(__file__).parent.parent / "configs" / f"{config_name}.json") as f:
+        config = json.load(f)
+    return config
 
 
 if __name__ == '__main__':
@@ -32,12 +46,7 @@ if __name__ == '__main__':
         location="europe-west1",
         enable_caching=False,
 
-        parameter_values={
-            "project_id": PROJECT_ID,
-            "country": "france",
-            "start_date": "2022-01-01",
-            "end_date": "2022-12-31"
-        },
+        parameter_values={**load_config("config_1")},
     )
 
     job.run(service_account=SERVICE_ACCOUNT)
